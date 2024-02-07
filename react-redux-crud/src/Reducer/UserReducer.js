0badca9ab27f9636068e35a6be7411243a68dbe8
AKSHAY SHARMA <jsakshay653@gmail.com>
2024-02-07 11:59:00 +0530

    started and almost completed react-redux-crud app, did json server also

---
+++ react-redux-crud/src/Reducer/UserReducer.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { createUser, readUser, updateUser, deleteUser } from '../Action/UserAction'
+
+// initial state
+
+// slice (reducer)
+
+// const UserSlice = createSlice({name, state, slice controller})
+const UserSlice = createSlice({
+    name: "user",
+    initialState: [],
+    extraReducers: (builder) => {
+        // addCase(action,reducer)
+         builder.addCase(createUser.fulfilled, (state,action) => {
+            if (action.payload) {
+                state.push(action.payload)   
+            }
+         })
+                .addCase(createUser.rejected, ()=> {})
+                .addCase(readUser.fulfilled, (state,action) => {
+                    return [...action.payload]
+                })
+                .addCase(readUser.rejected, () => {})
+                .addCase(updateUser.fulfilled, (state,action)=> {})
+                .addCase(updateUser.rejected, () => {})
+                .addCase(deleteUser.fulfilled, (state,action) => {
+                    const index = state.findIndex((item) => item.id === action.payload.id)
+                    state.splice(index, 1)
+                })
+                .addCase(deleteUser.rejected, () => {})
+    }
+})
+
+
+export default UserSlice
